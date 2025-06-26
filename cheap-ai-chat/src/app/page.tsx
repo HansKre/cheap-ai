324a6eb4c0ceaa4e659a19e2b2a12fb7f13eaa0a
HANS KREBS <hans.krebs@mercedes-benz.com>
2025-06-26 12:39:55 +0200

    Fix styling

--- cheap-ai-chat/src/app/page.tsx
+++ cheap-ai-chat/src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Chat() {
   const { messages, input, handleInputChange, handleSubmit } = useChat();
 
   return (
-    <div className="flex flex-col w-full mx-2 stretch">
+    <div className="flex flex-col w-screen h-screen px-8 stretch">
       {messages.map((m) => (
         <div key={m.id} className="whitespace-pre-wrap">
           {m.role === "user" ? "User: " : "AI: "}
@@ -15,9 +15,13 @@ export default function Chat() {
         </div>
       ))}
 
-      <form onSubmit={handleSubmit}>
+      <form
+        onSubmit={handleSubmit}
+        className="fixed bottom-0"
+        style={{ width: "calc(100% - 4rem)" }}
+      >
         <input
-          className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl text-black"
+          className="w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl text-black"
           value={input}
           placeholder="Say something..."
           onChange={handleInputChange}
