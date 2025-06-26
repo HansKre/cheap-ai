4358959abc0b21778918568d0c1912b4c0c09914
HANS KREBS <hans.krebs@mercedes-benz.com>
2025-06-26 13:02:39 +0200

    Limit width

--- cheap-ai-chat/src/app/page.tsx
+++ cheap-ai-chat/src/app/page.tsx
@@ -9,30 +9,41 @@ export default function Chat() {
 
   return (
     <div className="flex flex-col w-screen h-screen px-8 stretch">
-      {isLoading && (
-        <div className="py-4">
-          <p className="text-gray-500">Streaming...</p>
-        </div>
-      )}
-      {messages.map((m) => (
-        <div key={m.id} className="whitespace-pre-wrap">
-          {m.role === "user" ? "User: " : "AI: "}
-          <Markdown>{m.content}</Markdown>
-        </div>
-      ))}
+      <h1 className="text-2xl font-bold text-center my-4">Cheap AI Chat</h1>
+      {/* large screen content limiter */}
+      <div className="w-full max-w-[800px] mx-auto flex-1 flex flex-col">
+        {isLoading && (
+          <div className="py-4">
+            <p className="text-gray-500">Streaming...</p>
+          </div>
+        )}
+        {messages.map((m, i) => (
+          <div
+            key={m.id}
+            className={`flex whitespace-pre-wrap ${
+              m.role === "user" && i !== 0 ? "mt-6" : ""
+            }`}
+          >
+            <span className="flex-none min-w-[60px] text-gray-500 italic">
+              {m.role === "user" ? "User: " : "AI: "}
+            </span>
+            <Markdown className="flex-1">{m.content}</Markdown>
+          </div>
+        ))}
 
-      <form
-        onSubmit={handleSubmit}
-        className="fixed bottom-0"
-        style={{ width: "calc(100% - 4rem)" }}
-      >
-        <input
-          className="w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl text-black"
-          value={input}
-          placeholder="Say something..."
-          onChange={handleInputChange}
-        />
-      </form>
+        <form
+          onSubmit={handleSubmit}
+          className="fixed bottom-0"
+          style={{ width: "calc(100% - 4rem)" }}
+        >
+          <input
+            className="w-full max-w-[800px] p-2 mb-8 border border-gray-300 rounded shadow-xl text-black"
+            value={input}
+            placeholder="Say something..."
+            onChange={handleInputChange}
+          />
+        </form>
+      </div>
     </div>
   );
 }
